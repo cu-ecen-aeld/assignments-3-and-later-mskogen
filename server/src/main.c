30d453bca7f3cde8348ea10600e7feaf0a5c40fd
mskogen <theskogen@gmail.com>
2023-02-26 08:59:09 -0700

    Initial draft of aesdsocket with a the server connecting, reading, and sending to client connection.

---
+++ server/src/main.c
@@ -0,0 +1,28 @@
+/*****************************************************************************
+​*​ ​Copyright​ ​(C)​ ​2023 ​by​ Matthew Skogen
+​*
+​*​ ​Redistribution,​ ​modification​ ​or​ ​use​ ​of​ ​this​ ​software​ ​in​ ​source​ ​or​ ​binary
+​*​ ​forms​ ​is​ ​permitted​ ​as​ ​long​ ​as​ ​the​ ​files​ ​maintain​ ​this​ ​copyright.​ ​Users​ ​are
+​*​ ​permitted​ ​to​ ​modify​ ​this​ ​and​ ​use​ ​it​ ​to​ ​learn​ ​about​ ​the​ ​field​ ​of​ ​embedded
+​*​ ​software.​ ​Matthew Skogen ​and​ ​the​ ​University​ ​of​ ​Colorado​ ​are​ ​not​ ​liable​ ​for
+​*​ ​any​ ​misuse​ ​of​ ​this​ ​material.
+​*
+*****************************************************************************/
+/**
+​*​ ​@file​ main.c
+​*​ ​@brief​ Application Entry Point
+​*
+​*​ ​@author​s ​Matthew Skogen
+​*​ ​@date​ February 23 ​2023
+*
+*/
+
+#include "server.h"
+
+
+int main(int argc, char *argv[])
+{
+    test_server();
+
+    return 0;
+}
---
+++ server/src/server.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+​*​ ​Copyright​ ​(C)​ ​2023 ​by​ Matthew Skogen
+​*
+​*​ ​Redistribution,​ ​modification​ ​or​ ​use​ ​of​ ​this​ ​software​ ​in​ ​source​ ​or​ ​binary
+​*​ ​forms​ ​is​ ​permitted​ ​as​ ​long​ ​as​ ​the​ ​files​ ​maintain​ ​this​ ​copyright.​ ​Users​ ​are
+​*​ ​permitted​ ​to​ ​modify​ ​this​ ​and​ ​use​ ​it​ ​to​ ​learn​ ​about​ ​the​ ​field​ ​of​ ​embedded
+​*​ ​software.​ ​Matthew Skogen ​and​ ​the​ ​University​ ​of​ ​Colorado​ ​are​ ​not​ ​liable​ ​for
+​*​ ​any​ ​misuse​ ​of​ ​this​ ​material.
+​*
+*****************************************************************************/
+/**
+​*​ ​@file​ server.h
+​*​ ​@brief​ Server module for basic socket communication
+​*
+​*​ ​@author​s ​Matthew Skogen
+​*​ ​@date​ February 23 ​2023
+*
+*/
+
+
+int test_server();
