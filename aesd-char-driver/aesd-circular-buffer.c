fd2fe0fd5e6c4b9474b0484597019c37eb347249
mskogen <theskogen@gmail.com>
2023-03-07 20:21:15 -0700

    Working Circular Buffer implementation

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -17,6 +17,21 @@
 #include "aesd-circular-buffer.h"
 
 /**
+ * @param buffer the buffer to check if current in_off is at end of entry array
+ * @param offs_idx an index in the buffer entry list
+ * @return true if entry is the last entry in the entry array, false if not
+ */
+static bool end_entry(struct aesd_circular_buffer *buffer, uint8_t offs_idx)
+{
+    int buffer_size = (sizeof(buffer->entry)) / sizeof(struct aesd_buffer_entry);
+    
+    if (offs_idx == (buffer_size - 1)) {
+        return true;
+    }
+    return false;
+}
+
+/**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
  *      character index if all buffer strings were concatenated end to end
@@ -29,9 +44,71 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    size_t cur_offset = 0;
+    size_t cur_entry_offset = 0;
+    size_t entry_idx = 0;
+    struct aesd_buffer_entry* p_entry = NULL;
+
+    // If buffer is NULL, return NULL
+    if (buffer == NULL) {
+        return NULL;
+    }
+
+    entry_idx = buffer->out_offs;
+    p_entry = &(buffer->entry[entry_idx]);
+
+    // Ensure first entry isn't a null pointer
+    if (p_entry == NULL) {
+        return NULL;
+    }
+
+    // Walk through buffer until offset is reached
+    while (cur_offset < char_offset) {
+       
+        if (p_entry->size == 0 || (p_entry->buffptr == NULL)) {
+            // If size of entry is zero or buffer pointer is NULL, return NULL
+            return NULL;
+        } else if (cur_entry_offset == (p_entry->size - 1)) {
+            // If we are at the end of the current entry, go to next entry or 
+            // if we have wrapped all the way back around return NULL
+            if (end_entry(buffer, entry_idx)) {
+                // Wrap back to beginning entry
+                entry_idx = 0;
+            } else {
+                entry_idx++;
+            }
+
+            p_entry = &(buffer->entry[entry_idx]);
+
+            // Ensure first entry isn't a null pointer
+            if (p_entry == NULL) {
+                return NULL;
+            }
+
+            // If we are back to the beginning, return NULL, end of buffer
+            if (entry_idx == buffer->out_offs) {
+                return NULL;
+            }
+            
+            // Reset current entry offset for next entry buffer
+            cur_entry_offset = 0;
+        } else {
+            // Not at end of entry yet, increment offset
+            cur_entry_offset++;
+        }
+
+        cur_offset++;
+    }
+
+    // Successfully found entry
+    if (cur_offset == char_offset) {
+        // Verify entry isn't empty
+        if ((p_entry->size != 0) && (p_entry->buffptr != NULL)) {
+            *entry_offset_byte_rtn = cur_entry_offset;
+            return p_entry;
+        }
+    }
+
     return NULL;
 }
 
@@ -44,9 +121,31 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // No matter what we will write to current input offset
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    // Move input offset to next pointer, check if at end of entry array
+    if (end_entry(buffer, buffer->in_offs)) {
+        buffer->in_offs = 0;
+    } else {
+        buffer->in_offs++;
+    }
+
+    if (buffer->full) {
+        // If buffer was already full, make sure output offset moves with input
+        // offset
+        buffer->out_offs = buffer->in_offs;
+    } else {
+        // Check to see if buffer is now full for the first time
+        if (buffer->in_offs == buffer->out_offs) {
+            buffer->full = true;
+        } else {
+            // Buffer shouldn't be full, make sure flag isn't set to true
+            buffer->full = false;
+        }
+    }
+
+    return;
 }
 
 /**
@@ -55,4 +154,7 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
+    buffer->full = false;
+    buffer->in_offs = 0;
+    buffer->out_offs = 0;
 }
